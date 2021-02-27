744973a8faad425b851f62eb08904ab603beac63
alanpenacanosa <alanpenacanosa@gmail.com>
2021-02-27 14:36:03 +0100

    fix: changed the request module
    request-promise is deprecated, using node-fetch instead

--- ps5.js
+++ ps5.js
@@ -1,7 +1,7 @@
 const config = require('./config.json')
 const websites = require('./websites.json')
 
-const request = require('request-promise')
+const fetch = require('node-fetch');
 const jsdom = require('jsdom')
 const notifier = require('node-notifier')
 const open = require('open');
@@ -9,23 +9,25 @@ const chalk = require('chalk')
 
 function checkAvailability() {
     for (const website of websites) {
-        request(website.url).then(html => {
-            const document = new jsdom.JSDOM(html).window.document
-            const textFound = document.querySelector(website.selector).textContent
-            if (textFound === website.expecting) {
-                if (config.enableLogs) console.log(chalk.red(`${website.name} :\t${textFound}`))
-            } else {
-                if (config.enableLogs) console.log(chalk.green.bold(`${website.name} :\t${config.availableText}`))
-                notifier.notify({
-                    title: 'PS5 Scrapper',
-                    message: `${config.availableText} chez ${website.name}`,
-                    sound: true,
-                    wait: true
-                });
-                notifier.on('click', (notifierObject, options) => open(website.url))
-                open(website.url)
-            }
-        }).catch(err => console.error(chalk.bgRed(err)))
+        fetch(website.url)
+            .then(res => res.text())
+            .then(html => {
+                const document = new jsdom.JSDOM(html).window.document
+                const textFound = document.querySelector(website.selector).textContent
+                if (textFound === website.expecting) {
+                    if (config.enableLogs) console.log(chalk.red(`${website.name} :\t${textFound}`))
+                } else {
+                    if (config.enableLogs) console.log(chalk.green.bold(`${website.name} :\t${config.availableText}`))
+                    notifier.notify({
+                        title: 'PS5 Scrapper',
+                        message: `${config.availableText} chez ${website.name}`,
+                        sound: true,
+                        wait: true
+                    });
+                    notifier.on('click', (notifierObject, options) => open(website.url))
+                    open(website.url)
+                }
+            }).catch(err => console.error(chalk.bgRed(err)))
     }
 }
 
